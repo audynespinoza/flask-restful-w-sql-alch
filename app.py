60c7aabdb6c5e37241b99404ac2074cea9459d80
Audyn Espinoza <audyn.espinoza@ticketmaster.com>
2017-02-15 18:25:51 -0800

    modified files

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -12,7 +14,7 @@ from resources.store import Store, StoreList
 #the body of the POST will have additional data pertaining to the item
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 api=Api(app)
 app.secret_key = 'the_hard_to_guess_pwd'
